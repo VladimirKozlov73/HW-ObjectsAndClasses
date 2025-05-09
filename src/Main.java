e0a059623e0e8926becfc722fc7e07dab8211b75
VladimirKozlov73 <postwsz@mail.ru>
2025-05-09 20:46:37 +0300

    Сделано дз по объектам и классам

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Author author = new Author("Александр", "Блок");
+        Author author1 = new Author("Владимир", "Маяковский");
+        Book book = new Book("Седое утро", author, 1920);
+        Book book1 = new Book("Про это", author1, 1923);
+
+        book1.setYearBookPublication(1924);
     }
 }
